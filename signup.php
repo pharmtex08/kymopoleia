4758908e0973871f7f1178d4e414b4d1f2af2330
festusogundele <festusogundele9@gmail.com>
2019-10-03 05:24:49 +0100

    i work on the pwa

--- signup.php
+++ signup.php
@@ -79,6 +79,7 @@ if($user){
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="css/account.css">
         <script src="https://kit.fontawesome.com/833e0cadb7.js" crossorigin="anonymous"></script>
+         <link rel="manifest" href="manifest.json">
         <title>KymoBudget | Register</title>
     </head>
 
@@ -140,6 +141,7 @@ if($user){
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         <script src="./js/account.js" ></script>
+         <script src="js/app.js"></script>
     </body>
 
     </html>
